cd6ed30d2f7d604a5c601fdd86f864fba6d7b64d
yaur <dderkachev@gmail.com>
2020-04-28 17:15:55 +0300

    more edits

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,74 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
+import { tns } from  '../../node_modules/tiny-slider/src/tiny-slider'
 
-$(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
-})
+$(document).ready(function(){
+    var sliderView = tns({
+        container: '.slider-views',
+        items: 1,
+        loop: false,
+        nav: false,
+        mouseDrag: true,
+        arrowKeys: true,
+        controlsText: ['',''],
+        responsive: {
+          640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+          },
+          700: {
+            gutter: 30
+          },
+          900: {
+            items: 4
+          }
+        }
+    });
+
+    var sliderObserver = tns({
+        container: '.slider-observer',
+        items: 1,
+        loop: false,
+        nav: false,
+        mouseDrag: true,
+        arrowKeys: true,
+        controlsText: ['',''],
+        responsive: {
+          640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+          },
+          700: {
+            gutter: 30
+          },
+          900: {
+            items: 4
+          }
+        }
+    });
+
+    var sliderInfo = tns({
+        container: '.slider-info',
+        items: 1,
+        loop: false,
+        nav: false,
+        mouseDrag: true,
+        arrowKeys: true,
+        controlsText: ['',''],
+        responsive: {
+          640: {
+            edgePadding: 20,
+            gutter: 20,
+            items: 2
+          },
+          700: {
+            gutter: 30
+          },
+          900: {
+            gutter: 25,
+            items: 6
+          }
+        }
+    });
+});
